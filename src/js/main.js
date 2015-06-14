fa6c575765395c4b3e1554701523c55191222d75
Noah Yarian <noah.yarian@gmail.com>
2015-06-13 21:14:06 -0500

    added nav links, routes, and placeholder pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,18 @@
 var app = angular
-  .module("strangular", []);
+  .module("strangular", ['ngRoute'])
+
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/landing.html'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html'
+      })
+  });
